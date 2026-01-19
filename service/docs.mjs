dc58e2a3e7c9de30f0002712c3243b79cda828cf
Yves <yvart.pro@gmail.com>
2026-01-19 22:18:43 +0200

    feat: dashboard and endpoints url are exposed

--- service/docs.mjs
+++ service/docs.mjs
@@ -213,7 +213,7 @@ export const docsHtml = (endpoints, appUrl) => {
                     ${group.map(e => `
                         <div class="endpoint">
                             <span class="method ${e.method}">${e.method}</span>
-                            <a href="${appUrl}/mutama/${e.path}" class="path">/mutama/${e.path}</a>
+                            <a href="${appUrl}/${e.path}" class="path">/${e.path}</a>
                             <span class="desc">${e.description}</span>
                         </div>
                     `).join('')}
